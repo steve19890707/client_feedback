128623aa57ca9b25f79c2046ff800630c4711b3b
Steve <steve0639@cchntek.com>
2022-05-10 15:59:00 +0800

    Updates

---
+++ precache-manifest.c9c8b3ada36a57b2dbdbdf0a53782c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d246f4074910f24a9bc2e917f5eed9",
+    "url": "/client_feedback/index.html"
+  },
+  {
+    "revision": "a701df14c740b5108632",
+    "url": "/client_feedback/static/js/2.25480c93.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/client_feedback/static/js/2.25480c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daaf2c13f1d7bcaf34bf",
+    "url": "/client_feedback/static/js/main.81cac66e.chunk.js"
+  },
+  {
+    "revision": "3b7d947f0b3a129f87af",
+    "url": "/client_feedback/static/js/runtime-main.85ec7eea.js"
+  }
+]);
